8a841abb09f2df3209410b6671dfbfff7eea3f8c
Cannada Lewis <canlewi@localhost.localdomain>
2021-06-17 16:33:47 -0400

    Modified the type vector test to have more than one block and discovered my problem

--- mpi_put_with_type_vector/test.cpp
+++ mpi_put_with_type_vector/test.cpp
@@ -18,21 +18,24 @@ int main() {
   if (rank == 0) {
     std::vector<int> txt = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     MPI_Datatype vec_type;
-    MPI_Type_vector(3, 1, 3, MPI_INT, &vec_type);
+    MPI_Type_vector(6, 2, 3, MPI_INT, &vec_type);
     MPI_Type_commit(&vec_type);
 
     MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 1, 0, win);
-    MPI_Put(&txt[0], 1, vec_type, 1, 0, 3, MPI_INT, win);
+    MPI_Put(&txt[0], 1, vec_type, 1, 0, 6, MPI_INT, win);
     MPI_Win_unlock(1, win);
     MPI_Barrier(comm);
   } else if (rank == 1) {
     MPI_Barrier(comm);
-    int three[3];
+    int six[6];
     MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 1, 0, win);
-    MPI_Get(three, 3, MPI_INT, 1, 0, 3, MPI_INT, win);
+    MPI_Get(six, 6, MPI_INT, 1, 0, 6, MPI_INT, win);
     MPI_Win_unlock(1, win);
-    std::cout << "Rank 1 read: " << three[0] << ", " << three[1] << ", "
-              << three[2] << std::endl;
+    std::cout << "Rank 1 read: ";
+    for (auto i = 0; i < 6; ++i) {
+      std::cout << six[i] << " ";
+    }
+    std::cout << std::endl;
   } else {
     MPI_Barrier(comm);
   }
